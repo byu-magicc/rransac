48e504fb9f2dc1c7626af726978312417aa5855a
chadrs2 <crsamuelson87@gmail.com>
2020-09-01 14:59:13 -0600

    Added header and namespace formatting to measurement headers

--- include/rransac/common/measurement/measurement_factory.h
+++ include/rransac/common/measurement/measurement_factory.h
@@ -1,17 +1,24 @@
+#ifindef RRANSAC_COMMON_MEASUREMENT_MEASUREMENT_FACTORY_H_
+#define RRANSAC_COMMON_MEASUREMENT_MEASUREMENT_FACTORY_H_
+
 #include "rransac/common/measurement/measurement.h
 
-/** \class MeasurementFactory
- * Creates a new instant of a derived measurement class.
-*/
-
-class MeasurementFactory 
+namespace rransac
 {
-public:
+  /** \class MeasurementFactory
+   * Creates a new instant of a derived measurement class.
+  */
+  class MeasurementFactory
+  {
+  public:
 
-    /**
-     * Returns a unique pointer to a derived measurement class.
-     * @param[in] type The type of derived measurement class to use. 
-     * @see DerivedMeasurement
-     */
-    std::unique_ptr<MeasurementBase> CreateMeasurementClass(const DerivedMeasurement type);
-};
+      /**
+       * Returns a unique pointer to a derived measurement class.
+       * @param[in] type The type of derived measurement class to use.
+       * @see DerivedMeasurement
+       */
+      std::unique_ptr<MeasurementBase> CreateMeasurementClass(const DerivedMeasurement type);
+  };
+} // namespace rransac
+
+#endif // RRANSAC_COMMON_MEASUREMENT_MEASUREMENT_FACTORY_H_
