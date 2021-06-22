57425ae8ac56f2df497f973182298ab744388ea8
Mark Petersen <pmarke@byu.edu>
2021-06-22 17:13:01 -0600

    worked on the test for the trans se3 cam depth

--- include/rransac/common/transformations/trans_SE3_cam_depth.h
+++ include/rransac/common/transformations/trans_SE3_cam_depth.h
@@ -21,14 +21,15 @@ namespace rransac
 */
 
 template<class tState>
-class TransformSE3CamDepth : public TransformBase<Eigen::Matrix<typename tState::DataType,3,3>, tState, Eigen::Matrix<typename tState::DataType,TransformHomographyCovDim(tState::g_type_::dim_),TransformHomographyCovDim(tState::g_type_::dim_)>, TransformHomography<tState>> {
+class TransformSE3CamDepth : public TransformBase<Eigen::Matrix<typename tState::DataType,4,4>, tState, Eigen::Matrix<typename tState::DataType, 10,10>, TransformSE3CamDepth<tState>> {
 
 public:
 
 typedef typename tState::DataType DataType; /**< The scalar object for the data. Ex. float, double, etc. */
 typedef Eigen::Matrix<double,4,4> MatData;
+typedef Eigen::Matrix<double,10,10> MatCov;
 
-std::static_assert(std::is_same<tState,lie_groups::State<tState::G,tState::DataType,tState::N>::value, "TransformSE3CamDepth: The state is not supported");
+static_assert(std::is_same<tState,lie_groups::State< lie_groups::SE3,typename tState::DataType,tState::N>>::value, "TransformSE3CamDepth: The state is not supported");
 
 /**
  * Used to initialize the object, but it doesn't need to initialize anyting.
@@ -39,39 +40,59 @@ void DerivedInit() {};
  * Sets the data and the block components of the homography.
  * @param data The data required to transform the measurements, states, and error covariance
  */ 
-void DerivedSetData(const Mat3d data) {
+void DerivedSetData(const MatData data) {
     this->data_ = data;
 }
 
 /** 
  * Transforms the measurement from the previous tracking frame to the current one.
- * @param[in] meas The measurement to be transformed.
+ * @param[in,out] meas The measurement to be transformed.
  */ 
-void DerivedTransformMeasurement(Meas<DataType>& meas) const {
-
-    if (meas.twist.rows() != 0) {
-        Mat2d&& tmp = ConstructTranslationalVelTransform(meas.pose);
-        meas.twist = tmp*meas.twist;
-    }
-
-    meas.pose = TransformPosition(meas.pose);
-}
+void DerivedTransformMeasurement(Meas<DataType>& meas) const ;
 
 /** 
  * Transforms the track provided that the state is SE3_se3.
  * Since the velocity is expressed in the body frame, it doesn't need to be transformed.
  * Since the error covariance is on the error state, it doesn't need to be transformed. 
- * @param[in] state The track's state to be transformed.
- * @param[in] cov   The track's error covariance.
+ * @param[in,out] state The track's state to be transformed.
+ * @param[in,out] cov   The track's error covariance.
  */ 
 void DerivedTransformTrack(tState& state, MatCov& cov) const {
-    state.g_.data_ = this->data_*state_.g_.data_;
+    state.g_.data_ = this->data_*state.g_.data_;
 }
 
 
 
 
 };
+
+template<class tState>
+void TransformSE3CamDepth<tState>::DerivedTransformMeasurement(Meas<DataType>& meas) const {
+
+    DataType d_oa_a = meas.pose(0,0);
+    Eigen::Matrix<DataType,3,1> s_oa_a = meas.pose.block(1,0,3,1);
+    Eigen::Matrix<DataType,3,1> sd_oa_a = meas.twist;
+    Eigen::Matrix<DataType,3,3> I = Eigen::Matrix<DataType,3,3>::Identity();
+
+    Eigen::Matrix<DataType,3,3> R_ab = this->data_.block(0,0,3,3);
+    Eigen::Matrix<DataType,3,1> t_ab_b = this->data_.block(0,3,3,1);
+
+    Eigen::Matrix<DataType,3,1> t_oa_a = d_oa_a*s_oa_a;
+    Eigen::Matrix<DataType,3,1> t_ob_b = R_ab*t_oa_a + t_ab_b;
+    Eigen::Matrix<DataType,3,1> v_oa_a = (I*pow(d_oa_a,2) - t_oa_a*t_oa_a.transpose()).inverse()*pow(d_oa_a,3)*sd_oa_a;
+
+    DataType d_ob_b = sqrt(  (t_ob_b.transpose()*t_ob_b)(0,0)   );
+    Eigen::Matrix<DataType,3,1> s_ob_b = t_ob_b/d_ob_b;
+    Eigen::Matrix<DataType,3,1> sd_ob_b = R_ab*v_oa_a/d_ob_b - (t_ob_b*t_ob_b.transpose()*R_ab*v_oa_a)/pow(d_ob_b,3);
+
+    meas.pose(0,0) = d_ob_b;
+    meas.pose.block(1,0,3,1) = s_ob_b;
+    meas.twist = sd_ob_b;
+
+    std::cout << "v: " << std::endl << v_oa_a << std::endl;
+
+
+}
 
 
 
---
+++ test/common/transformations/trans_SE3_cam_depth_test.cpp
@@ -0,0 +1,78 @@
+#include <gtest/gtest.h>
+#include <chrono> 
+#include <iostream>
+
+#include "rransac/common/transformations/trans_SE3_cam_depth.h"
+#include "rransac/common/measurement/measurement_base.h"
+#include "rransac/common/sources/source_SE3_cam_depth.h"
+
+namespace rransac
+{
+
+using namespace lie_groups;
+
+
+TEST(TransformationSE3CamDepthTest, AllFunctions) {
+
+// Setup source
+typedef typename lie_groups::SE3_se3 State;
+typedef SourceSE3CamDepth<State> Source;
+
+SourceParameters params;
+params.type_ = MeasurementTypes::SE3_CAM_DEPTH;
+double noise = 0.1;
+params.meas_cov_ = Eigen::Matrix<double,7,7>::Identity()*noise;
+params.source_index_ = 0;
+Source source;
+source.Init(params);
+
+// Setup target
+Eigen::Matrix<double,10,10> cov, cov_transformed;
+cov.setRandom();
+cov_transformed = cov;
+State target, transform_data, target_transformed;
+target = State::Random();
+target_transformed = target;
+transform_data = State::Random();
+
+// Construct measurements
+Meas<double> m1, m2, m1_transformed;
+m1.source_index = 0;
+m2.source_index = 0;
+m1.type = MeasurementTypes::SE3_CAM_DEPTH;
+m2.type = MeasurementTypes::SE3_CAM_DEPTH;
+
+m1 = source.GetEstMeas(target);
+m1_transformed = m1;
+target_transformed.g_.data_ = transform_data.g_.data_*target_transformed.g_.data_;
+m2 = source.GetEstMeas(target_transformed);
+
+// Setup transform
+TransformSE3CamDepth<State> trans; 
+trans.SetData(transform_data.g_.data_);
+
+
+// Test the transform target
+trans.TransformTrack(target,cov);
+ASSERT_EQ(cov,cov_transformed);
+ASSERT_EQ(target.g_.data_, target_transformed.g_.data_);
+ASSERT_EQ(target.u_.data_, target_transformed.u_.data_);
+
+std::cout << "Rp" << std::endl << target.g_.R_*target.u_.p_ << std::endl;
+
+// Test the transform measurements
+trans.TransformMeasurement(m1_transformed);
+ASSERT_EQ(m1_transformed.pose, m2.pose);
+ASSERT_EQ(m1_transformed.twist, m2.twist);
+
+
+
+
+
+
+
+
+}
+
+    
+} // namespace rransac
